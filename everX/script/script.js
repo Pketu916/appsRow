a07fc01fc4079c8610c4c40a1b4ee88f7563ef1b
Ketu Patel <pketu916@gmail.com>
2025-05-26 18:31:29 +0530

    circle spin added

--- everX/script/script.js
+++ everX/script/script.js
@@ -1,39 +1,3 @@
-// document.addEventListener('DOMContentLoaded', () => {
-//   const hamburgerButton = document.querySelector('.hamburger-button');
-//   const navContainer = document.querySelector('.nav-container');
-//   const openIcon = document.querySelector('.open-icon');
-//   const closeIcon = document.querySelector('.close-icon');
-//   const navLinks = document.querySelectorAll('.nav-list a');
-//   const startButton = document.querySelector('.global-button');
-
-//   function showMenu() {
-//     navContainer.classList.add('show');
-//     openIcon.style.display = 'none';
-//     closeIcon.style.display = 'inline';
-//   }
-
-//   function hideMenu() {
-//     navContainer.classList.remove('show');
-//     openIcon.style.display = 'inline';
-//     closeIcon.style.display = 'none';
-//   }
-
-//   hamburgerButton.addEventListener('click', () => {
-//     const isMenuOpen = navContainer.classList.contains('show');
-//     if (isMenuOpen) {
-//       hideMenu();
-//     } else {
-//       showMenu();
-//     }
-//   });
-
-//   // Close menu on nav link or button click
-//   navLinks.forEach(link => {
-//     link.addEventListener('click', hideMenu);
-//   });
-
-//   startButton.addEventListener('click', hideMenu);
-// });
 document.addEventListener("DOMContentLoaded", () => {
   const hamburgerButton = document.querySelector(".hamburger-button");
   const navContainer = document.querySelector(".nav-container");
@@ -115,7 +79,7 @@ function initSwiper() {
         },
       },
     });
-  } else if (!isMobile && swiperInstance) {
+  } else if (!isMobile && !swiperInstance) {
     swiperInstance.destroy(true, true);
     swiperInstance = undefined;
   }
