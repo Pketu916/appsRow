665223057483a9db5595109644447e60f5a07e69
Ketu Patel <pketu916@gmail.com>
2025-06-24 15:01:27 +0530

    soma done

--- soma/assets/scripts/script.js
+++ soma/assets/scripts/script.js
@@ -1,5 +1,55 @@
 var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 3,
+  slidesPerView: 'auto',    // Allow flexible slide width
   spaceBetween: 20,
+  centeredSlides: true,
+  loop: false,
+  
+  breakpoints: {
+    992: {
+      slidesPerView: 3,
+      centeredSlides: false,
+    },
+    768: {
+      slidesPerView: 2,
+      centeredSlides: false,
+    },
+    0: {
+      slidesPerView: 1,
+      centeredSlides: false,
+    }
+  }
+});
+const menuBtn = document.querySelector('.menu');
+const nav = document.querySelector('.header-wrapper nav');
+const navLinks = nav.querySelectorAll('li a, .mobile-header-links a');
+const headerLinks = document.querySelectorAll('.header-links a');
+const dropdownToggles = document.querySelectorAll('.dropdown-container .dropdown');
+
+menuBtn.addEventListener('click', () => {
+    nav.classList.toggle('active');
+    menuBtn.classList.toggle('activeMenu');
 });
 
+// Close menu only on <a> clicks
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        nav.classList.remove('active');
+        menuBtn.classList.remove('activeMenu');
+    });
+});
+
+headerLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        nav.classList.remove('active');
+        menuBtn.classList.remove('activeMenu');
+    });
+});
+
+// Toggle dropdown independently
+dropdownToggles.forEach(toggle => {
+    toggle.addEventListener('click', (e) => {
+        e.stopPropagation(); // Prevent bubbling
+        const dropdownMenu = toggle.nextElementSibling;
+        dropdownMenu.classList.toggle('active');
+    });
+});
