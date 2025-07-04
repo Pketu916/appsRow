65d923369ea20c020c51441a63939525273e2e28
Ketu Patel <pketu916@gmail.com>
2025-07-04 14:59:44 +0530

    react slider add

--- react swiper/src/index.js
+++ react swiper/src/index.js
@@ -5,17 +5,18 @@ import App from './App';
 import WebflowSwiper from "./WebflowSwiper";
 
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App/>
-//   </React.StrictMode>
-// );
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App/>
+  </React.StrictMode>
+);
 
 
-const container = document.getElementById('react-swiper');
+// const container = document.getElementById('react-swiper');
 
-if (container) {
-  const root = ReactDOM.createRoot(container);
-  root.render(<WebflowSwiper />);
-}
+// if (container) {
+//   const root = ReactDOM.createRoot(container);
+//   root.render(<WebflowSwiper />);
+// }
