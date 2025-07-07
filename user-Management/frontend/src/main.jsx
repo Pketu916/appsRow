7fed830e4cab2f07db5d60fd4cdeca80dda111c8
Ketu Patel <pketu916@gmail.com>
2025-07-07 17:40:20 +0530

    build app

--- user-Management/frontend/src/main.jsx
+++ user-Management/frontend/src/main.jsx
@@ -3,8 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./store/AuthContext";
-import './index.css'
-
+import './index.css';
 
 // ReactDOM.createRoot(document.getElementById("root")).render(
 //   <React.StrictMode>
@@ -21,12 +20,12 @@ const container = document.getElementById('app');
 if (container) {
   const root = ReactDOM.createRoot(container);
   root.render(
-   <React.StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </BrowserRouter>
-  </React.StrictMode>
+    <React.StrictMode>
+      <BrowserRouter>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </BrowserRouter>
+    </React.StrictMode>
   );
 }
