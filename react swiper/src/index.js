7fe8d5c4a55bd5e31b57f5ebbdb8c5a62c610722
Ketu Patel <pketu916@gmail.com>
2025-07-07 12:11:05 +0530

    MetricsSection add

--- react swiper/src/index.js
+++ react swiper/src/index.js
@@ -7,12 +7,12 @@ import MetricsSection from "./MetricsSection";
 
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <MetricsSection/>
-  </React.StrictMode>
-);
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <MetricsSection/>
+//   </React.StrictMode>
+// );
 
 
 // const container = document.getElementById('react-swiper');
