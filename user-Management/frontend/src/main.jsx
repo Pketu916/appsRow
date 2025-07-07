099e0ab7a05c9700805fee235203696dec089502
Ketu Patel <pketu916@gmail.com>
2025-07-07 16:03:43 +0530

    build add

--- user-Management/frontend/src/main.jsx
+++ user-Management/frontend/src/main.jsx
@@ -6,12 +6,27 @@ import { AuthProvider } from "./store/AuthContext";
 import './index.css'
 
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <React.StrictMode>
+//     <BrowserRouter>
+//       <AuthProvider>
+//         <App />
+//       </AuthProvider>
+//     </BrowserRouter>
+//   </React.StrictMode>
+// );
+
+
+
+if (container) {
+  const root = ReactDOM.createRoot(container);
+  root.render(
+   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
         <App />
       </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
-);
+  );
+}
