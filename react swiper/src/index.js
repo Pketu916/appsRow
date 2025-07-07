d6301b23b4d0e290d747e16f488943ec4926cd3e
Ketu Patel <pketu916@gmail.com>
2025-07-07 12:08:08 +0530

    MetricsSection add

--- react swiper/src/index.js
+++ react swiper/src/index.js
@@ -1,22 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import WebflowSwiper from "./WebflowSwiper";
+// import App from './App';
+// import WebflowSwiper from "./WebflowSwiper";
+import MetricsSection from "./MetricsSection";
 
 
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App/>
-//   </React.StrictMode>
-// );
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <MetricsSection/>
+  </React.StrictMode>
+);
 
 
-const container = document.getElementById('react-swiper');
+// const container = document.getElementById('react-swiper');
+
+// if (container) {
+//   const root = ReactDOM.createRoot(container);
+//   root.render(<WebflowSwiper />);
+// }
+
+const container = document.getElementById('MetricsSection');
 
 if (container) {
   const root = ReactDOM.createRoot(container);
-  root.render(<WebflowSwiper />);
+  root.render(<MetricsSection />);
 }
