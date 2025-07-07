d885f0b1f9b86c463aa2095fdde5f4a60129b182
Ketu Patel <pketu916@gmail.com>
2025-07-07 12:28:44 +0530

    new section add

---
+++ Metrics Section/src/MetricsSection.jsx
@@ -0,0 +1,76 @@
+const MetricsSection = () => {
+  return (
+    <section className="section metrics">
+      <div className="container">
+        <div className="layout-metrics">
+          <h2
+            data-effect2=""
+            data-splitting=""
+            data-w-id="cafcb076-d8c5-6823-fbae-342808f4ce77"
+            style={{ opacity: 1 }}
+            className="h2"
+          >
+            Our numbers speak for themselves
+          </h2>
+
+          <div
+            data-w-id="cafcb076-d8c5-6823-fbae-342808f4ce79"
+            style={{ opacity: 1 }}
+            className="w-layout-grid layout-list-item"
+          >
+            <div className="layout-text-wrapper">
+              <div className="h2">4.25%</div>
+              <div className="paragraph">Annual percentage yield</div>
+            </div>
+
+            <div
+              id="w-node-cafcb076-d8c5-6823-fbae-342808f4ce7f-4cc48279"
+              className="vertical-divider"
+            ></div>
+
+            <div className="layout-text-wrapper">
+              <div className="h2">4.7/5</div>
+              <div className="paragraph">Customer satisfaction</div>
+            </div>
+
+            <div
+              id="w-node-cafcb076-d8c5-6823-fbae-342808f4ce85-4cc48279"
+              className="vertical-divider"
+            ></div>
+
+            <div className="layout-text-wrapper">
+              <div className="h2">43K+</div>
+              <div className="paragraph">No-fee ATMs</div>
+            </div>
+
+            <div
+              id="w-node-cafcb076-d8c5-6823-fbae-342808f4ce8b-4cc48279"
+              className="vertical-divider"
+            ></div>
+
+            <div className="layout-text-wrapper">
+              <div className="h2">0%</div>
+              <div className="paragraph">Commission for bonds</div>
+            </div>
+          </div>
+        </div>
+
+        <img
+          loading="lazy"
+          src="https://cdn.prod.website-files.com/6866252e3e7f51ad4cc48270/686b695cbf8b5d7ad35e0c1c_Background%20Gradient.svg"
+          alt=""
+          className="bg-gradient right"
+        />
+
+        <img
+          loading="lazy"
+          src="https://cdn.prod.website-files.com/6866252e3e7f51ad4cc48270/686b695cbf8b5d7ad35e0c1c_Background%20Gradient.svg"
+          alt=""
+          className="bg-gradient"
+        />
+      </div>
+    </section>
+  );
+};
+
+export default MetricsSection;
