8c3c81b8e62767a923e67d4efc39875e12442179
Ketu Patel <pketu916@gmail.com>
2025-07-11 10:28:02 +0530

    user management

--- user-Management/frontend/src/main.jsx
+++ user-Management/frontend/src/main.jsx
@@ -5,27 +5,27 @@ import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./store/AuthContext";
 import './index.css';
 
-// ReactDOM.createRoot(document.getElementById("root")).render(
-//   <React.StrictMode>
-//     <BrowserRouter>
-//       <AuthProvider>
-//         <App />
-//       </AuthProvider>
-//     </BrowserRouter>
-//   </React.StrictMode>
-// );
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
 
-const container = document.getElementById('app');
+// const container = document.getElementById('app');
 
-if (container) {
-  const root = ReactDOM.createRoot(container);
-  root.render(
-    <React.StrictMode>
-      <BrowserRouter>
-        <AuthProvider>
-          <App />
-        </AuthProvider>
-      </BrowserRouter>
-    </React.StrictMode>
-  );
-}
+// if (container) {
+//   const root = ReactDOM.createRoot(container);
+//   root.render(
+//     <React.StrictMode>
+//       <BrowserRouter>
+//         <AuthProvider>
+//           <App />
+//         </AuthProvider>
+//       </BrowserRouter>
+//     </React.StrictMode>
+//   );
+// }
